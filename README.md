ff072e09d7f5c8b18a72e656939650803b32a51b
Mofe <moflux620@gmail.com>
2022-05-02 07:49:56 +0100

    Learning to work with APIs

---
+++ README.md
@@ -0,0 +1,32 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/Mofe620)
+- Twitter - [@yourusername](https://www.twitter.com/Mofe620)
---
+++ script.js
@@ -0,0 +1,34 @@
+let aID = document.querySelector("#advice-id");
+let aContent = document.querySelector("#advice-content");
+let dice = document.querySelector(".dice");
+
+document.onload = getAdvice();
+
+dice.addEventListener('click', function(){
+    getAdvice();
+})
+
+function getAdvice(){
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://api.adviceslip.com/advice', true)
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response) //data is now an object. 'this.response' was a string
+        var adviceSlip = data.slip
+        if (request.status >= 200 && request.status < 400) {
+            aID.textContent = "#" + adviceSlip.id;
+            aContent.textContent = '"' + adviceSlip.advice + '"';
+        }   else {
+                console.log('error')
+            }
+
+    }
+
+    // Send request
+    request.send()
+
+}
